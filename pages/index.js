01cd30ff39ebf829a9bab10d8db7918a6e5c1265
rb718 <ghisodye@hotmail.com>
2023-03-28 15:52:18 -0500

    caused sanity-client error, trying to fix it

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,37 @@
 import React from "react";
 
+import { client, client1 } from "@/lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
       <HeroBanner />
+      {console.log(bannerData)}
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
