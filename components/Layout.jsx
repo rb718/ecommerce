3b1d48dd73de1765ade19127f7118372f2e5e361
rb718 <ghisodye@hotmail.com>
2023-03-30 14:25:33 -0500

    Footer done & initial Navbar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Sanity Ecommerce Store</title>
       </Head>
       <header>
         <Navbar />
